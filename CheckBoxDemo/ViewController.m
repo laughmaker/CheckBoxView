e5a90f012480aa09760f354dfcfc89afa31e089b
makeLaugh <wskrdfeai@gmail.com>
2013-06-09 14:38:17 +0800

    完成代码重构

--- CheckBoxDemo/ViewController.m
+++ CheckBoxDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "CheckBoxView.h"
 
 @interface ViewController ()
 
@@ -17,8 +18,52 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    self.checkboxes = [[NSMutableArray alloc] initWithCapacity:10];
+    
+    CheckBoxView *cbv = nil;
+    CGRect frame = CGRectMake(20, 20, 240, 30);
+    for (int i = 0; i < 10; ++i)
+    {
+        CheckBoxViewStyle style = (i % kCheckBoxViewStylesCount);
+        BOOL checked = (i < 5);
+        cbv = [[CheckBoxView alloc] initWithFrame:frame style:style checked:checked];
+        [cbv setText:[NSString stringWithFormat:@"Option #%02d", (i + 1)]];
+        [self.view addSubview:cbv];
+        [self.checkboxes addObject:cbv];
+
+        frame.origin.y += 36;
+    }
+    
+    frame.origin.y += 24;
+    cbv = [[CheckBoxView alloc] initWithFrame:frame  style:kCheckBoxViewStyleGlossy checked:YES];
+    [cbv setText:@"Enable All"];
+    
+    [cbv addStateChangedTarget:self selector:@selector(checkBoxViewChangedState:)];
+    
+    // handle state changed event using blocks
+//    
+//     __block typeof(self) me = self;
+//     [cbv setStateChangedBlock:^(CheckBoxView *v)
+//      {
+//          [me checkBoxViewChangedState:v];
+//     }];
+    
+    
+    [self.view addSubview:cbv];
 }
+
+- (void)checkBoxViewChangedState:(CheckBoxView *)cbv
+{
+    NSLog(@"checkBoxViewChangedState: %d", cbv.checked);
+    
+    // toggle all
+    for (CheckBoxView *cbv in self.checkboxes)
+    {
+        cbv.enabled = !cbv.enabled;
+    }
+}
+
 
 - (void)didReceiveMemoryWarning
 {
